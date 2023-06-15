070cb18c9d6cc09b40c12ab779c75e1f0e7b3b41
Habtamu <habtamuwolde96@gmail.com>
2023-06-15 09:42:44 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,9 @@
  *
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	char mesg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1, mesg, sizeof(mesg)-1);
-	return (0);
+	write(1, mesg, sizeof(mesg) - 1);
+	return (1);
 }
