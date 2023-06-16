89b57535e11a46a4bdb95f55e75ff0eb2b3d9c51
Habtamu <habtamuwolde96@gmail.com>
2023-06-16 03:22:23 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -5,12 +5,13 @@
  * Return: Always 0 (success)
 */
 int main(void)
-{       
-        for(int i=0; i<26; i++)
-        {       
-                putchar('z' - i);
-        }       
-        putchar('\n');
-        
-        return(0);
-}   
+{
+	int i;
+
+	for(i = 0; i < 26; i++)
+	{
+		putchar('z' - i);
+	}
+	putchar('\n');
+	return(0);
+}
