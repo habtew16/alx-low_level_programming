8ab663147051a207ef1d792034df87e6e5948469
Habtamu <habtamuwolde96@gmail.com>
2023-06-16 03:19:42 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,13 @@
  * Return: Always 0 (success)
 */
 int main(void)
-{       
-        for(int i=0; i<10; i++)
-        {       
-                putchar('0' + i);
-        }       
-        putchar('\n');
-        
-        return(0);
-} 
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
