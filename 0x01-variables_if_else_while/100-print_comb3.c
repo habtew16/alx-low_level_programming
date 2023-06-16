604f9a479ee93f254aaa60c7a240668c55b071e7
Habtamu <habtamuwolde96@gmail.com>
2023-06-16 03:41:27 -0700

    space required fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int i;
 	int j;
 
-	for(i = 0; i <= 8; i++)
+	for (i = 0; i <= 8; i++)
 	{
-		for(j = i + 1; j <= 9; j++)
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
-			if(i != 8 || j != 9)
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
