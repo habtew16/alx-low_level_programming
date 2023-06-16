06e5d5a8bc25517a520a0532d2627499049efddd
Habtamu <habtamuwolde96@gmail.com>
2023-06-16 03:06:42 -0700

    print alphab

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,19 +5,19 @@
  * Return: Always 0 (success)
 */
 int main(void)
-{       
-        for(int i=0; i<26; i++)
-        {
+{
+	int i;
+        for (i = 0; i < 26; i++)
+	{
 		if( i == 4 || i == 16)
 		{
 			continue;
 		}
 		else
 		{
-                	putchar('a' + i);
+			putchar('a' + i);
 		}
-        }       
-        putchar('\n');
-        
-        return(0);
-}   
+	}
+	putchar('\n');
+	return (0);
+}
