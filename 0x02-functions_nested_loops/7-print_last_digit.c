3d237702f4416f3fb97e407e3101200f72484c0f
Habtamu <habtamuwolde96@gmail.com>
2023-06-20 02:32:43 -0700

    last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,16 @@
 #include "main.h"
+#include <stdlib.h>
 
+/**
+ * print_last_digit - entry point
+ * @num: number
+ * Return: last digit of num
+*/
 int print_last_digit(int num)
 {
-	return (num % 10);
+	int lastD;
+
+	lastD = abs(num % 10);
+	_putchar('0' + lastD);
+	return (lastD);
 }
