a5980edfd6eec3bab445c786053fe948b3b6cd16
Habtamu <habtamuwolde96@gmail.com>
2023-06-22 02:29:38 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,25 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%i ", i);
+			printf("%i", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
+	printf("\n");
 	return (0);
 }
