440466bf526e981728def20bc0fd7caa6984dcfe
Habtamu <habtamuwolde96@gmail.com>
2023-06-26 09:41:38 -0700

    password generator

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -5,26 +5,27 @@
 #define PASSWORD_LENGTH 8
 
 char generateRandomChar() {
-    // Generate a random character within the ASCII range of printable characters
-    return (rand() % 94) + 33;  // Printable ASCII characters range from 33 to 126
+	return (rand() % 94) + 33;
 }
 
-void generateRandomPassword(char *password) {
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = generateRandomChar();
-    }
-    password[PASSWORD_LENGTH] = '\0';  // Add null-terminating character
+void generateRandomPassword(char *password)
+{
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = generateRandomChar();
+	}
+	password[PASSWORD_LENGTH] = '\0';
 }
 
 int main() {
-    srand(time(NULL));  // Initialize the random number generator with the current time
-    
-    char password[PASSWORD_LENGTH + 1];  // +1 for the null-terminating character
-    
-    generateRandomPassword(password);
-    
-    printf("Random password: %s\n", password);
-    
-    return 0;
+	srand(time(NULL));
+
+	char password[PASSWORD_LENGTH + 1];
+
+	generateRandomPassword(password);
+
+	printf("Random password: %s\n", password);
+
+	return 0;
 }
 
