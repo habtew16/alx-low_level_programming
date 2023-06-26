654c8f167c03cda098e8ad2de44e992406403db3
Habtamu <habtamuwolde96@gmail.com>
2023-06-26 10:49:47 -0700

    generate password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,35 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 8
-
-char generateRandomChar(void)
-{
-	return ((rand() % 94) + 33);
-}
-
-void generateRandomPassword(char *password)
-{
-	int i;
-
-
-	for (i = 0; i < PASSWORD_LENGTH; i++)
-	{
-		password[i] = generateRandomChar();
-	}
-	password[PASSWORD_LENGTH] = '\0';
-}
-
+/**
+ * main - entry pont
+ * return 0 on sucees
+*/
 int main(void)
 {
+	int pass[100];
+	int i, sum, n;
+
+	sum = 0;
+
 	srand(time(NULL));
 
-	char password[PASSWORD_LENGTH + 1];
-
-	generateRandomPassword(password);
-
-	printf("Random password: %s\n", password);
-
+	for(i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78);
+		{
+			n = 2772 - sum -'0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
 	return (0);
 }
-
