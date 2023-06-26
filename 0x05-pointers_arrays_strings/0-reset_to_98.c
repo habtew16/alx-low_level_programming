46239a572287777713dd74d2b56074d0f50acc06
Habtamu <habtamuwolde96@gmail.com>
2023-06-26 00:50:06 -0700

    pointers, arrays, strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - entry point which changes value var
+ * @n: is poinnter variable
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "mainn.h"
+
+/**
+ * _putchar - used to print one char at a time
+ * @c: is variable used to to pass characters
+ * Return: 1 on success
+ * -1 on failor
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
