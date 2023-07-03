a609a596d0ff304b00b0b584839fc72985a42aa4
Habtamu <habtamuwolde96@gmail.com>
2023-07-03 02:00:23 -0700

    0x07 more pointer arrays and strings

---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - entry point to print chaacters
+ * @c: variable c
+ * Return: returns 1 if success and
+ * 0 if fails
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c. 1));
+}
