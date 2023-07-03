0e02688c429ae9d02932f6ce77dcb800e26e4bba
Habtamu <habtamuwolde96@gmail.com>
2023-07-03 02:09:06 -0700

    copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - function to copy memeroy area from srcc to dest
+ * @dest: destination memory area
+ * @src: source memory area
+ * Return: returns new dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
