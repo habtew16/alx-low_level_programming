878e220d6bc2e0fa16cda2c74183687be949f3db
Habtamu <habtamuwolde96@gmail.com>
2023-07-03 09:44:54 -0700

    set string

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * set_string - sets string using pointer
+ * @s: is string to be modified
+ * @to: string to replace anything in s
+ * no return
+*/
+
 void set_string(char **s, char *to)
 {
 	*s = to;
