5bc94132cd7593e4d3c1592ce7ce1404d22aa1c3
Habtamu <habtamuwolde96@gmail.com>
2023-07-07 09:28:37 -0700

    add iteratively

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - entry point for functions
+ * @argC: length for argV
+ * @argV: collection of arguments
+ * Return: 0 if success
+ * 1 not success
+*/
+
+int main(int argC, char *argV[])
+{
+	int i;
+
+	if (argC == 1)
+	{
+		printf("%d\n", 0);
+	}
+	else
+	{
+		int sum;
+
+		sum = 0;
+		for (i = 1; i < argC; i++)
+		{
+
+			if (!isdigit(*argV[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argV[i]);
+			}
+		}
+		printf("%d\n", sum);
+	}
+
+	return (0);
+}
