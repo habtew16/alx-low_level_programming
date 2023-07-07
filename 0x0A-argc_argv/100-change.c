3421acd9918b69f18c0be76a064bdf369c4cb5d8
Habtamu <habtamuwolde96@gmail.com>
2023-07-07 10:27:14 -0700

    change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * @argC: length of arguments
+ * @argV: argv is collection of arguments
+ * Return: 0 if success
+ * 1 if fails
+ */
+
+int main(int argC, char  *argV[])
+{
+	int coins;
+	int money;
+
+	coins = 0;
+
+	if (argC == 2)
+	{
+		if (strchr(argV[argC - 1], '-'))
+		{
+			printf("0\n");
+			return (1);
+		}
+
+		money = atoi(argV[argC - 1]);
+
+		while (money > 0)
+		{
+			if (money % 25 == 0)
+			{
+				money -= 25;
+			} else if (money % 10 == 0)
+			{
+				money -= 10;
+			} else if (money % 5 == 0)
+			{
+				money -= 5;
+			} else if (money % 2 == 0)
+			{
+				money -= 2;
+			} else
+			{
+				money--;
+			}
+			coins++;
+		}
+		printf("%d\n", coins);
+		return (0);
+	}
+	printf("Error\n");
+	return (1);
+}
