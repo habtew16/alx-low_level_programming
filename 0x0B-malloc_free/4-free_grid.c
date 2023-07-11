2a9aeeaf9a8e359b0d656cee8d8a38ac2329e564
Habtamu <habtamuwolde96@gmail.com>
2023-07-11 02:12:17 -0700

    free grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * free_grid - frees memorey occupied by grid
+ * @grid: is grid
+ * @height: is the number of rows of grid
+ * no return
+*/
+
+
 void free_grid(int **grid, int height)
 {
 	int i;
