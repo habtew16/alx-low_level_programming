2a9ce3d847f1172051df8368bc23458fda977746
Habtamu <habtamuwolde96@gmail.com>
2023-07-11 00:19:10 -0700

    betty style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,22 +31,22 @@ char *str_concat(char *s1, char *s2)
 
 	ptr = malloc(sizeof(char) * (len1 + strlen(s2) + 1));
 
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 
-	for(i = 0; s1[i] != '\0';  i++)
+	for (i = 0; s1[i] != '\0';  i++)
 	{
 		len1++;
 		ptr[i] = s1[i];
 	}
 
-	for(i = 0; s2[i] != '\0'; i++)
+	for (i = 0; s2[i] != '\0'; i++)
 	{
 		ptr[i + len1] = s2[i];
 	}
 
-
+	ptr[i + len1] = '\0';
 	return (ptr);
 }
