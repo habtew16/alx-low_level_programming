c8ae1cc4a470f13d5ad3512a58a3958dce667098
Habtamu <habtamuwolde96@gmail.com>
2023-07-13 08:14:57 -0700

    fix for calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	void *ptr = malloc(nmemb * size);
+	char *ptr = malloc(nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
