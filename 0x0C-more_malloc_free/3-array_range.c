3a4aaf5ac0d1322826413d1d93a51dafaf80c470
Habtamu <habtamuwolde96@gmail.com>
2023-07-13 08:36:42 -0700

    array rnge

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates array in range of given nnumbers
+ * inncluding both upper and lower limits
+ * @min: lower limit
+ * @max: upper limit
+ * Return: returns ptr if succes else
+ * null is returned
+*/
+
+int *array_range(int min, int max)
+{
+	int i;
+	int *ptr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = (int *)malloc(sizeof(int *) * (min + max + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = min; i <= max; i++)
+	{
+		ptr[i] = i;
+	}
+	return (ptr);
+}
