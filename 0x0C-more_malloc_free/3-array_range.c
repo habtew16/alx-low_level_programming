a5b712f6ef6015bec9476d40dae6913241ba1e22
Habtamu <habtamuwolde96@gmail.com>
2023-07-13 08:44:31 -0700

    fixed memory allocation error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,18 +15,18 @@ int *array_range(int min, int max)
 	int i;
 	int *ptr;
 
+	len = 0;
 	if (min > max)
 	{
 		return (NULL);
 	}
 
-	ptr = (int *)malloc(sizeof(int *) * (min + max + 1));
+	ptr = (int *)malloc(sizeof(int *) * (max - min + 1));
 
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = min; i <= max; i++)
 	{
 		ptr[i] = i;
