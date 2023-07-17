c6d76e7efda6eda4321f82ab976ef8a24a4fca67
Habtamu <habtamuwolde96@gmail.com>
2023-07-16 23:39:18 -0700

    c - preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point to print name
+ * prints name of the compiled file
+ * Return: 0 in always
+ */
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef function_like_macro_h
+#define function_like_macro_h
+
+#define ABS(a) (((a) < 0) ? -(a) : (a))
+
+
+#endif
+
