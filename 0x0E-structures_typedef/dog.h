b2386227ce5af7b6794870524421ddcedebf7682
Habtamu <habtamuwolde96@gmail.com>
2023-07-17 03:37:07 -0700

    struct data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - user defined data type for dog
+ *
+ *
+ * @name: the nae of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
