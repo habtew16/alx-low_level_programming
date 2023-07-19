574001309502737ae159637f29e3f0d00aa8629e
Habtamu <habtamuwolde96@gmail.com>
2023-07-19 03:18:59 -0700

    print array of numbers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * array_iterator - function to iterate over aray and
  * print eleents of array
+ * @array: array of numbers
  * @size: size of an array
  * @action: pointer to function which prints int
 */
@@ -11,8 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (action != NULL && size > 0)
 	{
-		(*action)(array[i]);
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
 	}
 }
