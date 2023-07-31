3a71a8f0a85accd50c7e652e68f9685a9726dcd9
Habtamu <habtamuwolde96@gmail.com>
2023-07-31 11:19:44 -0700

    pop the top element

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * pop_listint - function to pop the first
+ * element of the list
+ * @head: head of the list
+ * Return: returns the popped element
+ */
+
+int pop_listint(listint_t **head)
+{
+	int popped;
+	listint_t *current;
+
+	current = *head;
+
+	popped = current->n;
+	current = current->next;
+	*head = current;
+
+	return (popped);
+}
