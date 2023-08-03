89c504a77ba60cf04ccec298480e1202caf461ae
Habtamu <habtamuwolde96@gmail.com>
2023-08-03 11:00:53 -0700

    binnary to unnit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,23 +10,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num;
-
-	num = 0;
+	unsigned int num = 0;
 
 	if (b == NULL)
 		return (0);
-
-	while (*b != '\0')
+	while (*b)
 	{
-		if (*b != '1' && *b != '0')
+		if (*b == '1')
+			num = (num << 1) | 1;
+		else if (*b == '0')
+			num <<= 1;
+		else
 			return (0);
-
-		num <<= 1;
-
-		num += (*b - '0');
 		b++;
 	}
-
 	return (num);
 }
