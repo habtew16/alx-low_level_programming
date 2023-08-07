e3f5a8eee2e7ae78dc82036f3c0dfcf5908f9989
Habtamu <habtamuwolde96@gmail.com>
2023-08-07 12:09:47 -0700

    fixed problem with exit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,14 +15,14 @@ int copy_file(const char *src_file, const char *dest_file)
 	if (fdFrum == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
-		return (98);
+		exit(98);
 	}
 	fdToo = open(dest_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fdToo == -1)
 	{
 		close(fdFrum);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
-		return (99);
+		exit(99);
 	}
 
 	while ((readed = read(fdFrum, buff, 1024)) > 0)
@@ -33,7 +33,7 @@ int copy_file(const char *src_file, const char *dest_file)
 			close(fdFrum);
 			close(fdToo);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest_file);
-			return (99);
+			exit(99);
 		}
 	}
 	if (readed == -1)
@@ -41,7 +41,7 @@ int copy_file(const char *src_file, const char *dest_file)
 		close(fdFrum);
 		close(fdToo);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src_file);
-		return (98);
+		exit(98);
 	}
 	close(fdFrum);
 	close(fdToo);
@@ -59,11 +59,14 @@ int copy_file(const char *src_file, const char *dest_file)
 
 int main(int ac, char **av)
 {
+	int result;
+
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
-	return (copy_file(av[1], av[2]));
+	result = copy_file(av[1], av[2]);
+	return (result);
 }
 
