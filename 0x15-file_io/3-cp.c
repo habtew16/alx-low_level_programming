7bcd2322c25393ff09c61e3a1b9074e8d232b597
Habtamu <habtamuwolde96@gmail.com>
2023-08-26 02:40:31 -0700

    fixed error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,54 +1,4 @@
 #include "main.h"
-#include <errno.h>
-/**
-* copyFile - Copies the content from one file to another.
-* @fromPath: Path to the source file.
-* @toPath: Path to the destination file.
-* Return: 0 on success, -1 on failure.
-*/
-int copyFile(const char *fromPath, const char *toPath)
-{
-	int fdFrum, fdToo, wrote, readed;
-	char buff[1024];
-
-	fdFrum = open(fromPath, O_RDONLY);
-	if (fdFrum == -1)
-	{
-		return (-1);
-	}
-
-	fdToo = open(toPath, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fdToo == -1)
-	{
-		close(fdFrum);
-		return (-1);
-	}
-
-	while ((readed = read(fdFrum, buff, 1024)) > 0)
-	{
-		wrote = write(fdToo, buff, readed);
-		if (wrote == -1)
-		{
-			close(fdFrum);
-			close(fdToo);
-			return (-1);
-		}
-	}
-
-	if (readed == -1)
-	{
-		close(fdFrum);
-		close(fdToo);
-		return (-1);
-	}
-
-	if (close(fdFrum) == -1 || close(fdToo) == -1)
-	{
-		return (-1);
-	}
-
-	return (0);
-}
 
 /**
  * main - Main entry to copy file's content to another file.
@@ -56,34 +6,52 @@ int copyFile(const char *fromPath, const char *toPath)
  * @av: Total array of arguments.
  * Return: 0 on success, else exit value on failure.
  */
-
 int main(int ac, char **av)
 {
+	int fdFrum, fdToo, wrote, readed;
+	char buff[1024];
+
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	if (copyFile(av[1], av[2]) == -1)
+	fdFrum = open(av[1], O_RDONLY);
+	if (fdFrum == -1)
 	{
-		if (errno == EACCES)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fdToo = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fdToo == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	while ((readed = read(fdFrum, buff, 1024)) > 0)
+	{
+		wrote = write(fdToo, buff, readed);
+		if (wrote == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Permission denied\n");
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			exit(99);
 		}
-		else if (errno == ENOENT)
-		{
-			dprintf(STDERR_FILENO, "Error: No such file or directory\n");
-			exit(98);
-		}
-		else
-		{
-			dprintf(STDERR_FILENO, "Error: An error occurred\n");
-			exit(100);
-		}
 	}
-
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (close(fdFrum) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdFrum);
+		exit(100);
+	}
+	if (close(fdToo) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdToo);
+		exit(100);
+	}
 	return (0);
 }
 
