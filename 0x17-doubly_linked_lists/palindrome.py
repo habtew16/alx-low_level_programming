cae70e8bc68826d71d4c1ffa7189fb97c99d3016
Habtamu <habtamuwolde96@gmail.com>
2023-09-14 10:41:46 -0700

    largest palindrom nnnumeber

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def ispalindrome(n):
+    s = str(n)
+    return s == s[::-1]
+
+li = []
+for i in range(999, 99,-1):
+    for j in range(i, 99, -1):
+        if ispalindrome(i * j):
+            li.append(i * j)
+print(max(li))
