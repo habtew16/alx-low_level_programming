272389ef011c627606ed8968d73d9b087ad99c45
Habtamu <habtamuwolde96@gmail.com>
2023-09-14 10:14:54 -0700

    insert nnnode in given index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+
+/**
+ * insert_dnodeint_at_index -funnction to insert item in the given idx
+ * @h: head of the doubl linked list
+ * @idx: index where node to be inserted
+ * @n: number to innnserted
+ * Return: address of new node or null if failed
+ */
+
+
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current, *new;
+	unsigned int index;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if ((*h) == NULL)
+	{
+		if (idx == 0)
+		{
+			(*h) = new;
+			return (new);
+		}
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		new->next = *h;
+		(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+	current = *h;
+	index = 1;
+	while (current->next != NULL && index != idx)
+	{
+		current = current->next;
+		index++;
+	}
+	if (index == idx)
+	{
+		new->prev = current;
+		new->next = current->next;
+
+		if (current->next != NULL)
+			current->next->prev = new;
+		current->next = new;
+		return (new);
+	}
+	return (NULL);
+}
