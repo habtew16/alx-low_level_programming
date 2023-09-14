077e07e6c9aa51ba4db3d9e56203cfab382d0f1c
Habtamu <habtamuwolde96@gmail.com>
2023-09-14 10:46:23 -0700

    paldinrome

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -3,8 +3,9 @@ def ispalindrome(n):
     s = str(n)
     return s == s[::-1]
 
+
 li = []
-for i in range(999, 99,-1):
+for i in range(999, 99, -1):
     for j in range(i, 99, -1):
         if ispalindrome(i * j):
             li.append(i * j)
